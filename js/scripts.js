b7581c06b3d99c0c61343e92a35e2466a8257784
James Cho <hwisoocho@gmail.com>
2018-10-19 16:29:44 -0700

    add punctuation

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
         var suggest3;
         var address;
         
+
         if (name) {
             $("span#nameOutput").text(name + ", ");
             }
@@ -30,54 +31,54 @@ $(document).ready(function(){
             address = "600 1st Ave, Seattle, WA "
         } else if (campus === "Portland") {
             $("#campus-link").attr("href", "https://www.epicodus.com/portland/");
-            $("#campus-map").attr("href", "https://www.google.com/maps/place/400+SW+Sixth+Avenue,+400+SW+6th+Ave,+Portland,+OR+97204/")
+            $("#campus-map").attr("href", "https://www.google.com/maps/place/400+SW+Sixth+Avenue,+400+SW+6th+Ave,+Portland,+OR+97204/");
             address = "400 SW 6th Ave, Portland, OR "
         }
 
         if(build === "internal"){
-            suggest1 = "C#/.NET"
+            suggest1 = "C#/.NET";
         } else if(build === "dynamic"){
-            suggest1 = "PHP/Drupal"
+            suggest1 = "PHP/Drupal";
         } else if (build === "android"){
-            suggest1 = "Java/Android"
+            suggest1 = "Java/Android";
         } else if (build === "interactive"){
-            suggest1 = "Ruby/Rails"
+            suggest1 = "Ruby/Rails";
         } 
 
         if(company === "bigger" && suggest1 != "C#/.NET"){
-            suggest2 = "C#/.NET"
+            suggest2 = "C#/.NET";
         } else if (company === "fast-paced" && suggest1 != "PHP/Drupal"){
-            suggest2 = "PHP/Drupal"
+            suggest2 = "PHP/Drupal";
         } else if (company === "enterprise" && suggest1 != "Java/Android"){
-            suggest2 = "Java/Android"
+            suggest2 = "Java/Android";
         } else if (company === "start-up" && suggest1 != "Ruby/Rails"){
-            suggest2 = "Ruby/Rails"
+            suggest2 = "Ruby/Rails";
         } else {
-            suggest2 = "CSS/Design"
+            suggest2 = "CSS/Design";
         }
 
         if (side ==="front" && suggest2 != "CSS/Design") {
-            suggest3 = "CSS/Design"
+            suggest3 = "CSS/Design";
         } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "C#/.NET") {
-            suggest3 = "C#/.NET"
+            suggest3 = "C#/.NET";
         } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "PHP/Drupal") {
-            suggest3 = "PHP/Drupal"
+            suggest3 = "PHP/Drupal";
         } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "Java/Android") {
-            suggest3 = "Java/Android"
+            suggest3 = "Java/Android";
         } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "Ruby/Rails") {
-            suggest3 = "Ruby/Rails"
+            suggest3 = "Ruby/Rails";
         }
 
         if (side === "back" &&  suggest1 != "C#/.NET" && suggest2 !="C#/.NET") {
-            suggest3 = "C#/.NET"
+            suggest3 = "C#/.NET";
         } else if (side === "back" && suggest1!= "PHP/Drupal" && suggest2 != "PHP/Drupal") {
-            suggest3 = "PHP/Drupal"
+            suggest3 = "PHP/Drupal";
         } else if (side === "back" && suggest1 != "Java/Android" && suggest2 != "Java/Android") {
-            suggest3 = "Java/Android"
+            suggest3 = "Java/Android";
         } else if (side === "back" && suggest1 != "Ruby/Rails" && suggest2 != "Ruby/Rails") {
-            suggest3 = "Ruby/Rails"
+            suggest3 = "Ruby/Rails";
         } else if(side === "back" && suggest1 != "CSS/Design" && suggest2 != "CSS/Design") {
-            suggest3 = "CSS/Design"
+            suggest3 = "CSS/Design";
         }
 
        clickSound.play();
