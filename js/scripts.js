382777c361d6f951d5271307a2b632c4f00ee064
James Cho <hwisoocho@gmail.com>
2018-10-19 11:25:52 -0700

    add suggest3 logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,36 +14,61 @@ $(document).ready(function(){
         var suggest2;
         var suggest3;
 
-        if(company === "bigger"){
+        if(build === "internal"){
             suggest1 = "C#/.NET"
-        } else if (company === "fast-paced"){
+        } else if(build === "dynamic"){
             suggest1 = "PHP/Drupal"
-        } else if (company === "enterprise") {
+        } else if (build === "android"){
             suggest1 = "Java/Android"
-        } else if (company === "start-up") {
+        } else if (build === "interactive"){
             suggest1 = "Ruby/Rails"
-        }
+        } 
 
-        if(build === "internal" && company != "bigger"){
+        if(company === "bigger" && suggest1 != "C#/.NET"){
             suggest2 = "C#/.NET"
-        } else if(build === "dynamic" && company != "fast-paced"){
+        } else if (company === "fast-paced" && suggest1 != "PHP/Drupal"){
             suggest2 = "PHP/Drupal"
-        } else if (build === "android" && company != "enterprise"){
+        } else if (company === "enterprise" && suggest1 != "Java/Android"){
             suggest2 = "Java/Android"
-        } else if (build === "interactive" && company != "start-up"){
+        } else if (company === "start-up" && suggest1 != "Ruby/Rails"){
             suggest2 = "Ruby/Rails"
         } else {
             suggest2 = "CSS/Design"
         }
 
+        if (side ==="front" && suggest2 != "CSS/Design") {
+            suggest3 = "CSS/Design"
+        } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "C#/.NET") {
+            suggest3 = "C#/.NET"
+        } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "PHP/Drupal") {
+            suggest3 = "PHP/Drupal"
+        } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "Java/Android") {
+            suggest3 = "Java/Android"
+        } else if (side === "front" && suggest2 === "CSS/Design" && suggest1 != "Ruby/Rails") {
+            suggest3 = "Ruby/Rails"
+        }
 
-        
-        console.log(build);
+        if (side ==="back" && suggest2 != "CSS/Design") {
+            suggest3 = "CSS/Design"
+        } else if (side === "back" && suggest2 === "CSS/Design" && suggest1 != "C#/.NET") {
+            suggest3 = "C#/.NET"
+        } else if (side === "back" && suggest2 === "CSS/Design" && suggest1 != "PHP/Drupal") {
+            suggest3 = "PHP/Drupal"
+        } else if (side === "back" && suggest2 === "CSS/Design" && suggest1 != "Java/Android") {
+            suggest3 = "Java/Android"
+        } else if (side === "back" && suggest2 === "CSS/Design" && suggest1 != "Ruby/Rails") {
+            suggest3 = "Ruby/Rails"
+        }
+
+            console.log(build);
 
         $("div#output").hide();
-        $("span#nameOutput").text(name);
+        if (name) {
+        $("span#nameOutput").text(name + ", ");
+        }
         $("span#suggest1").text(suggest1);
         $("span#suggest2").text(suggest2);
+        $("span#suggest3").text(suggest3);
         $("div#output").fadeIn(2000);
         
     })
